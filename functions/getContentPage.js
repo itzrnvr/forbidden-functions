1a1c56184d9f8b9d531d97e8b4c19aab5dfb1b7d
Anatame <anata.me97@gmail.com>
2023-01-19 18:52:04 +0530

    first function

---
+++ functions/getContentPage.js
@@ -0,0 +1,12 @@
+async function fetchHtml() {
+    const response = await fetch('https://9anime.to')
+    const html = await response.text()
+    return html
+  }
+
+exports.handler = async function(event, context, callback){
+    callback(null, {
+        statusCode: 200,
+        body: await fetchHtml()
+    })
+}
